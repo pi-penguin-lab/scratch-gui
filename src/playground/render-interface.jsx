69203c3ef9e46fa3e2ba977520dcd623f661ff9a
wigmasterrrrrrrrrrrr <wigmasterrrrrrrrrrrr@users.noreply.github.com>
2026-07-29 01:53:35 -0400

    remove project input from studio

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -31,7 +31,6 @@ import SettingsStore from '../addons/settings-store-singleton';
 import '../lib/tw-fix-history-api';
 import GUI from './render-gui.jsx';
 import MenuBar from '../components/menu-bar/menu-bar.jsx';
-import ProjectInput from '../components/tw-project-input/project-input.jsx';
 import FeaturedProjects from '../components/tw-featured-projects/featured-projects.jsx';
 import Description from '../components/tw-description/description.jsx';
 import BrowserModal from '../components/browser-modal/browser-modal.jsx';
@@ -262,9 +261,6 @@ class Interface extends React.Component {
                             {isBrowserSupported() ? null : (
                                 <BrowserModal isRtl={isRtl} />
                             )}
-                            <div className={styles.section}>
-                                <ProjectInput />
-                            </div>
                             {(
                                 // eslint-disable-next-line max-len
                                 description.instructions === 'unshared' || description.credits === 'unshared'
